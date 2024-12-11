f0da136520fee178c40af639135b218336fdc99c
yucef raifa <yucefr@gmail.com>
2024-12-11 16:38:08 +0100

    push working

--- Adherents/index.php
+++ Adherents/index.php
@@ -1518,8 +1518,8 @@ if ($profil == 4) {; ?>
             const remainingAmount = parseFloat(resteInput.value.replace(' MAD', '')) || 0;
             const totalAmount = parseFloat(totalInput.value.replace(' MAD', '')) || 0;
 
-            console.log(totalInput)
-            if (remainingAmount <= 0 && totalInput !== 0) {
+            console.log(totalAmount)
+            if (remainingAmount <= 0 && totalAmount !== 0) {
                 alert("Le montant total a été atteint. Vous ne pouvez pas ajouter un autre mode de paiement.");
                 return;
             }
